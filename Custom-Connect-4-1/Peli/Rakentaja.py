ad5b97e1fdad4178973bf848a2f094ddbb28eadc
Deadlyweather <tonyruotsalainenuusi@gmail.com>
2025-05-27 10:01:43 +0300

    Delete Custom-Connect-4-1 directory

--- Custom-Connect-4-1/Peli/Rakentaja.py
+++
@@ -1,9 +0,0 @@
-import Settings
-with open("Peli/Settings.py", "w") as reset:
-    pass
-with open("Peli/Lauta","a") as Lauta:
-    for X in range(Settings.leveys):
-        for Y in range(Settings.korkeus):
-            Lauta.write("#")
-        Lauta.write("\n")
-
--- Custom-Connect-4-1/Peli/Settings.py
+++
@@ -1,5 +0,0 @@
-class Settings:
-    def __init__(self, leveys=8, korkeus=8, määrä=4):
-        self.leveys = leveys
-        self.korkeus = korkeus
-        self.määrä = määrä
