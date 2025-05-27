6f17000eb992386dbfa097ea376468e67e4b2858
Tony Ruotsalainen <tonyruotsalainenuusi@gmail.com>
2025-05-27 10:18:20 +0300

    Update

--- Peli/Pelaa.py
+++ Peli/Pelaa.py
@@ -3,7 +3,7 @@ print("Settings:")
 while True:
     Valinnat = Settings.Settings(input("Anna leveys: "), input("Anna korkeus: "), input("Anna määrä: "))
     print(Valinnat)
-    if input("Oletko tyytyväinen valintoihisi? (k/e): ").lower() == 'k':
+    if input("Oletko tyytyväinen valintoihisi?: ").lower() == 'kyllä':
         break
 while True:
     print("pelaaja 1:")
