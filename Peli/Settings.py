0b8a53fc5f13c374eb8e51e0191c21b3fdea8744
Deadlyweather <tonyruotsalainenuusi@gmail.com>
2025-05-27 08:40:25 +0300

    Add files via upload

---
+++ Peli/Settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    def __init__(self):
+        self.settings = {}
+    def lauta(self, leveys, korkeus):
+        self.leveys = leveys
+        self.korkeus = korkeus
+    def voiton_ehto(self, määrä):
+        self.määrä = määrä
+    
