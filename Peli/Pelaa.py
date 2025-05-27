d8b42badb56ff9c420c1e6fe4adb6dd2eb4126b0
Tony Ruotsalainen <tonyruotsalainenuusi@gmail.com>
2025-05-27 10:30:06 +0300

    Fixing

--- Peli/Pelaa.py
+++ Peli/Pelaa.py
@@ -5,5 +5,15 @@ while True:
     print(Valinnat)
     if input("Oletko tyytyväinen valintoihisi?: ").lower() == 'kyllä':
         break
-while True:
-    print("pelaaja 1:")
+print("Aloitetaan peli...")
+with open("Peli/Settings.py", "w") as reset:
+    pass
+with open("Peli/Lauta","a") as Lauta:
+    for X in range(Settings.leveys):
+        for Y in range(Settings.korkeus):
+            Lauta.write("#")
+        Lauta.write("\n")
+print("Lauta on luotu.")
+with open("Peli/Lauta") as Lauta:
+    for rivi in Lauta:
+        print(rivi.strip())
--- Peli/Settings.py
+++
@@ -1,7 +0,0 @@
-class Settings:
-    def __init__(self, leveys=8, korkeus=8, määrä=4):
-        self.leveys = leveys
-        self.korkeus = korkeus
-        self.määrä = määrä
-    def __str__(self):
-        return f"Leveys: {self.leveys}, Korkeus: {self.korkeus}, Määrä: {self.määrä}"
