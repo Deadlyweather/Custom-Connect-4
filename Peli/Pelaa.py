06f31086137f28b30a03b38a17a3858334122a80
onni <holttaonni@gmail.com>
2025-05-28 08:23:23 +0300

    Update Pelaa.py

--- Peli/Pelaa.py
+++ Peli/Pelaa.py
@@ -4,19 +4,21 @@ def kysy_asetukset():
         try:
             leveys = int(input("Anna leveys: "))
             korkeus = int(input("Anna korkeus: "))
-            maara = int(input("Montako peräkkäin voittaa?: "))
+            maara = int(input("Anna määrä: "))
         except ValueError:
             print("Syötä kokonaisluku!")
             continue
         if input("Oletko tyytyväinen valintoihisi? (k/e): ").lower() == 'k':
             break
-    return leveys, korkeus, maara
+    return leveys, korkeus, määrä
+
+leveys, korkeus, maara = kysy_asetukset()
 print("Aloitetaan peli...")
 with open("Peli/Settings.py", "w") as reset:
     pass
 with open("Peli/Lauta","a") as Lauta:
-    for X in range(Settings.leveys):
-        for Y in range(Settings.korkeus):
+    for X in range(leveys):
+        for Y in range(korkeus):
             Lauta.write("#")
         Lauta.write("\n")
 print("Lauta on luotu.")
